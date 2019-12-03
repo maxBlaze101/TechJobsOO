c2d608993971b8efe88c5c95faf57bc137873ed7
Max <maxblaze101@gmail.com>
2019-12-03 16:39:48 -0500

    trasfered working TechJobsOO
    transfered working TechjobsOO from desktop PC

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,8 +21,10 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job somejob = jobData.Find(id);
+            //Type not appropriate for JobFieldsViewModel
+            //if somejob fed in: "The model item passed into the ViewDataDictionary is of type 'TechJobs.Models.Job', but this ViewDataDictionary instance requires a model item of type 'TechJobs.ViewModels.JobFieldsViewModel'.
+            return View(somejob);
         }
 
         public IActionResult New()
@@ -35,7 +39,24 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Employer anEmployer = jobData.Employers.Find(newJobViewModel.EmployerID);
+                Location aLocation = jobData.Locations.Find(newJobViewModel.LocationID);
+                CoreCompetency aSkill = jobData.CoreCompetencies.Find(newJobViewModel.SkillId);
+                PositionType aPositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeId);
 
+                Job newjob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = anEmployer,
+                    Location = aLocation,
+                    CoreCompetency = aSkill,
+                    PositionType = aPositionType
+                };
+                jobData.Jobs.Add(newjob);
+                return Redirect (String.Format("/Job?id={0}", newjob.ID));
+            }
             return View(newJobViewModel);
         }
     }
---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string Title { get; set; } = "";
+        public JobFieldType Column { get; set; } = JobFieldType.All;
+        public List<JobFieldType> Columns { get; set; } 
+    }
+}
